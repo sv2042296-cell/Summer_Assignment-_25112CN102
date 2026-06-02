1ebf1c5354b2f9b23d9ce08b14e539d13ac72f4d
shivam verma <sv2042296@gmail.com>
2026-06-02 21:28:59 +0530

    reverse of number

---
+++ Summer_Assignment-_25112CN102/Day2/Q2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter your number=";
+    cin>>n;
+    int lastdigit;
+    int reverse=0;
+    while(n>0){
+        lastdigit=n%10;
+        reverse=reverse*10+lastdigit;
+        n=n/10;
+
+    }
+    cout<<reverse;
+}
