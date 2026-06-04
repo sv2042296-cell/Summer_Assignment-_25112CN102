100b79398cef1cdf8d3aaeb438e194b96399f3ca
shivam verma <sv2042296@gmail.com>
2026-06-04 16:31:31 +0530

    Armstrong numbers in a range.

---
+++ Summer_Assignment-_25112CN102/Day4/Q4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int start, end;
+    cout << "Enter start and end: ";
+    cin >> start >> end;
+    for (int i = start;  i<= end;i++) {
+        int temp = i;
+        int sum = 0;
+
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += digit * digit * digit;
+            temp /= 10;
+        }
+
+        if (sum ==i) {
+            cout <<i<< " ";
+        }
+    }
+
+    return 0;
+}
