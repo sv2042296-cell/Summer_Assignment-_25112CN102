c16866d8ec0ed43f09c6be88ce554c6f266f19c5
shivam verma <sv2042296@gmail.com>
2026-06-04 16:23:23 +0530

    fibonacci series

--- Summer_Assignment-_25112CN102/Day4/Q1.cpp
+++ Summer_Assignment-_25112CN102/Day4/Q1.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main() {
     int n;
-    cout << "Enter your term : ";
+    cout << "Enter your terms: ";
     cin >> n;
     int a = 0, b = 1;
-    cout << "Fibonacci series: ";
-    for(int i=1;i<= n;i++) {
+    cout << "Fibonacci Seri: ";
+    for(int i = 1; i <= n; i++) {
         cout << a << " ";
         int c = a + b;
         a = b;
