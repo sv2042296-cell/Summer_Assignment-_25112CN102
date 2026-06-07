857314ccf81c87068c1a9d78aeb6944368d3dfdb
shivam verma <sv2042296@gmail.com>
2026-06-07 20:36:09 +0530

    reverse no

---
+++ Summer_Assignment-_25112CN102/Day7/Q4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int reverseNum(int n, int rev = 0) {
+    if (n == 0)
+        return rev;
+    return reverseNum(n / 10, rev * 10 + n % 10);
+}
+int main() {
+    int n;
+    cout<<"Enter your no:";
+    cin >> n;
+    cout << reverseNum(n);
+    return 0;
+}
