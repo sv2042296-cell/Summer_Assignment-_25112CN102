2e031e588257b41e44c97dcef2d15147b3ec4afd
shivam verma <sv2042296@gmail.com>
2026-06-08 21:27:55 +0530

    repeated-number

--- Summer_Assignment-_25112CN102/Day8.cpp/Q4.cpp
+++ Summer_Assignment-_25112CN102/Day8.cpp/Q4.cpp
@@ -5,11 +5,10 @@ int main() {
     cout << "Enter number of rows: ";
     cin >> n;
     for(int i = 1; i <= n; i++) {
-        for(char ch = 'A'; ch < 'A' + i; ch++) {
-            cout << ch;
+        for(int j = 1; j <= i; j++) {
+            cout << i;
         }
         cout << endl;
     }
-
     return 0;
 }
