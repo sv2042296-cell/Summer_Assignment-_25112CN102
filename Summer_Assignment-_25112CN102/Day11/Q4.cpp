b4cafa6eb19afa63c80726f18b8811ab2a5e6a18
shivam verma <sv2042296@gmail.com>
2026-06-11 21:26:25 +0530

    factorial through function

--- Summer_Assignment-_25112CN102/Day11/Q4.cpp
+++ Summer_Assignment-_25112CN102/Day11/Q4.cpp
@@ -1,15 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int factorial(int n ){
-    int fact=1;
-    for(int i=1;i<=n;i++){
-        fact=fact*i;
+int factorial(int n)
+{
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        fact = fact * i;
     }
     return fact;
 }
-int main(){
+int main()
+{
     int n;
-    cout<<"Enter your no:";
-    cin>>n;
-    cout<<"factorial of "<<""<<n <<"="<<factorial(n);
+    cout << "Enter your no:";
+    cin >> n;
+    cout << "factorial of " << "" << n << "=" << factorial(n);
 }
