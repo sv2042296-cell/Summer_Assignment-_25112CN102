1d95a04d2ad824781528d6caa11a2f07b0b776b5
shivam verma <sv2042296@gmail.com>
2026-06-11 21:23:29 +0530

    max no

--- Summer_Assignment-_25112CN102/Day11/Q2.cpp
+++ Summer_Assignment-_25112CN102/Day11/Q2.cpp
@@ -1,18 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int Max(int a,int b){
-    if(a>b){
-        return a ;
+int Max(int a, int b)
+{
+    if (a > b)
+    {
+        return a;
     }
-    else {
+    else
+    {
         return b;
     }
 }
-int main(){
-    int a,b;
-    cout<<"value  of  a =";
-    cin>>a;
-    cout<<"value  of  b =";
-    cin>>b;
-    cout<<"This is th maximum no:"<<Max(a,b);    
+int main()
+{
+    int a, b;
+    cout << "value  of  a =";
+    cin >> a;
+    cout << "value  of  b =";
+    cin >> b;
+    cout << "This is th maximum no:" << Max(a, b);
 }
