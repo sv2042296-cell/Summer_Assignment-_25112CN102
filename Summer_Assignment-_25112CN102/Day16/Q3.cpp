31f338799d78325397b4b05bcc0b5ac98b9a526e
shivam verma <sv2042296@gmail.com>
2026-06-16 21:09:46 +0530

    2sum

--- Summer_Assignment-_25112CN102/Day16/Q3.cpp
+++ Summer_Assignment-_25112CN102/Day16/Q3.cpp
@@ -1,25 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-    bool sum(int *arr,int n, int target){
-    for(int i=0;i<n;i++){
-        for(int j=1+i;j<n;j++){
-            if(arr[i]+arr[j]==target){
+bool sum(int *arr, int n, int target)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1 + i; j < n; j++)
+        {
+            if (arr[i] + arr[j] == target)
+            {
                 return true;
-            }     
+            }
         }
     }
     return false;
 }
-int main(){
+int main()
+{
     int n;
-    cout<<"Enter the size your array=";
-    cin>>n;
+    cout << "Enter the size your array=";
+    cin >> n;
     int target;
-    cout<<"enter your target=";
-    cin>>target;
+    cout << "enter your target=";
+    cin >> target;
     int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
- cout<<sum(arr,n,target);
+    cout << sum(arr, n, target);
 }
