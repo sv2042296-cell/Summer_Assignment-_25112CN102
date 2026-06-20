c58b88b6b5b74109a8d923c2b9c85cffb93da21f
shivam verma <sv2042296@gmail.com>
2026-06-20 20:19:27 +0530

    sub of two matrix

---
+++ Summer_Assignment-_25112CN102/Day19/Q2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n, m;
+
+  cout << "Enter number of rows: ";
+  cin >> n;
+
+  cout << "Enter number of columns: ";
+  cin >> m;
+
+  int A[n][m], B[n][m], Sum[n][m];
+
+  cout << "Enter elements of first matrix:\n";
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
+      cin >> A[i][j];
+    }
+  }
+
+  cout << "Enter elements of second matrix:\n";
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
+      cin >> B[i][j];
+    }
+  }
+
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
+      Sum[i][j] = A[i][j] - B[i][j];
+    }
+  }
+
+  cout << "\nsubtract of matrices:\n";
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
+      cout << Sum[i][j] << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
