1a784d42a3e62b0996909bba6e1aa47f5aba120e
shivam verma <sv2042296@gmail.com>
2026-06-20 21:02:09 +0530

    multiple of two matrix

---
+++ Summer_Assignment-_25112CN102/Day20/Q2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter order of matrix: ";
+    cin >> n;
+
+    int a[n][n];
+
+    cout << "Enter matrix elements:\n";
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    bool symmetric = true;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(a[i][j] != a[j][i]) {
+                symmetric = false;
+                break;
+            }
+        }
+    }
+
+    if(symmetric)
+        cout << "Matrix is Symmetric";
+    else
+        cout << "Matrix is Not Symmetric";
+
+    return 0;
+}
