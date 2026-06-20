940478cd0a56971cbe6555d7702bc119ee99080b
shivam verma <sv2042296@gmail.com>
2026-06-20 20:07:16 +0530

    Rename Day19 to Day18

--- Summer_Assignment-_25112CN102/Day19/Q1.cpp
+++ Summer_Assignment-_25112CN102/Day19/Q1.cpp
@@ -2,28 +2,42 @@
 using namespace std;
 
 int main() {
-    int n;
-    cout << "Enter size of array: ";
+    int n, m;
+
+    cout << "Enter number of rows: ";
     cin >> n;
 
-    int arr[n];
+    cout << "Enter number of columns: ";
+    cin >> m;
 
-    cout << "Enter elements: ";
+    int A[n][m], B[n][m], Sum[n][m];
+
+    cout << "Enter elements of first matrix:\n";
     for(int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-
-    for(int i = 0; i < n - 1; i++) {
-        for(int j = 0; j < n - i - 1; j++) {
-            if(arr[j] > arr[j + 1]) {
-                swap(arr[j], arr[j + 1]);
-            }
+        for(int j = 0; j < m; j++) {
+            cin >> A[i][j];
         }
     }
 
-    cout << "Sorted Array: ";
+    cout << "Enter elements of second matrix:\n";
     for(int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+        for(int j = 0; j < m; j++) {
+            cin >> B[i][j];
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            Sum[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    cout << "\nSum of matrices:\n";
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << Sum[i][j] << " ";
+        }
+        cout << endl;
     }
 
     return 0;
