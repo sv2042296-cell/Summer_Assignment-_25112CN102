f5608c670bf8976fffcc2f88ca96c39513c41643
shivam verma <sv2042296@gmail.com>
2026-06-29 21:48:52 +0530

    Menu-Driven Calculator

---
+++ Summer_Assignment-_25112CN102/Day29/Q1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int choice;
+    float a, b;
+    do {
+        cout << "\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exit\n";
+        cout << "Enter choice: ";
+        cin >> choice;
+        if (choice >= 1 && choice <= 4) {
+            cout << "Enter two numbers: ";
+            cin >> a >> b;
+        }
+        switch (choice) {
+            case 1: cout << "Result = " << a + b; break;
+            case 2: cout << "Result = " << a - b; break;
+            case 3: cout << "Result = " << a * b; break;
+            case 4:
+                if (b != 0)
+                    cout << "Result = " << a / b;
+                else
+                    cout << "Division by zero not allowed";
+                break;
+            case 5: cout << "Exiting..."; break;
+            default: cout << "Invalid Choice";
+        }
+    } while (choice != 5);
+
+    return 0;
+}
